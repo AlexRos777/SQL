4139420f4f0798f28efdb6f301d1bafa73284528
AlexRos777 <98416756+AlexRos777@users.noreply.github.com>
2022-03-30 22:21:46 +0300

    Update HW_1.sql

--- HW_1.sql
+++ HW_1.sql
@@ -17,7 +17,7 @@
  9. Вывести пользователей которые были созданы 2021-03-26 00:00:00
 	select*from students where created_on = '2021-03-26 00:00:00';
  10. Вывести пользователей где в имени есть слово Анна
-	select*from students where name like 'Anna%';
+	select*from students where name like '%Anna%';
  11. Вывести пользователей где в имени в конце есть 8
 	select*from students where name like '%8';
  12. Вывести пользователей где в имени в есть буква а
